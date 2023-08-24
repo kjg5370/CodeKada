fc612556f12ef2509f9838fef370b1f6d8a5ba33
kjg5370 <kjk03024@naver.com>
2023-08-24 10:00:35 +0900

    홀수짝수구하기

---
+++ algorithm/EvenOdd.cs
@@ -0,0 +1,24 @@
+﻿namespace algorithm
+{
+    internal class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+            Solution sol1 = new Solution();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{sol1.solution(i)}");
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(int num)
+        {
+            string answer = "";
+            answer = num % 2 == 0 ? "Even" : "Odd";
+            return answer;
+        }
+    }
+}
