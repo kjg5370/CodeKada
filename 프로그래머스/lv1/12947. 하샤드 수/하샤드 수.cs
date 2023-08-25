bfafa9e552f19eba1ce1f8bed952a2cdbe612772
kjg5370 <105926662+kjg5370@users.noreply.github.com>
2023-08-25 18:39:36 +0900

    [level 1] Title: 하샤드 수, Time: 0.23 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool solution(int x) {
+        bool answer = true;
+        int num = x;
+        int sum =0;
+        while(num > 0)
+        {
+            sum += num % 10;
+            num /=10;
+        }
+        if(x % sum != 0)
+            answer = false;
+        return answer;
+    }
+}
