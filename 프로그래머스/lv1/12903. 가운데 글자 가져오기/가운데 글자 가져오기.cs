8587f200e922cf3a0e85c2d141d77a70f7b369cb
kjg5370 <105926662+kjg5370@users.noreply.github.com>
2023-08-29 09:07:15 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.29 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+        if(s.Length % 2 != 0)
+            answer = s.Substring(s.Length / 2,1);
+        else
+            answer = s.Substring(s.Length /2-1, 2);
+        return answer;
+    }
+}
