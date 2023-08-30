b1a3d6485a89365d40ff1d54ba8aaabb19364b97
kjg5370 <105926662+kjg5370@users.noreply.github.com>
2023-08-30 09:51:02 +0900

    [level 1] Title: 삼총사, Time: 0.23 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        int n = number.Length;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = number[i] + number[j] + number[k];
+                    if(sum ==0)answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
