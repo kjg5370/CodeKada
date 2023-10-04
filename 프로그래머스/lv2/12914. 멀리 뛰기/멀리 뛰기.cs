6c847bb7b4c0d9fded58f8bb2094b689c03eac00
kjg5370 <105926662+kjg5370@users.noreply.github.com>
2023-10-04 09:09:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.24 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public long solution(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        
+        const long MOD = 1234567;
+        long[] dp = new long[n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        return dp[n];
+    }
+}
