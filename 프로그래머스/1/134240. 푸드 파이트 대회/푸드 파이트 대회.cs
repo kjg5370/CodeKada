a031566127a0f8e2bf20f7c7e80f070514eaa1a9
kjg5370 <105926662+kjg5370@users.noreply.github.com>
2024-01-03 14:06:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.23 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(int[] food) {
+        StringBuilder answer = new StringBuilder();
+        int foodCount = -1;
+        for(int i =0; i <food.Length; i++)
+        {
+            foodCount += 1;
+            for(int j = 0; j <food[i]/2; j++)
+            {
+                answer.Append(foodCount);                
+            }
+        }
+        answer.Append(0);  
+        for(int i =food.Length-1; i >=0; i--)
+        {
+            for(int j = 0; j <food[i]/2; j++)
+            {
+                answer.Append(foodCount);                
+            }
+            foodCount -= 1;
+        }
+        return answer.ToString();
+    }
+}
