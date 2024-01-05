92173c638890ea92e95ee97d31c51799f67a9a07
kjg5370 <105926662+kjg5370@users.noreply.github.com>
2024-01-05 14:09:45 +0900

    [level 1] Title: 햄버거 만들기, Time: 2.71 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cs
@@ -1,23 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
         List<int> list = new List<int>();
-        foreach(int burger in ingredient)
+        foreach (int burger in ingredient)
         {
             list.Add(burger);
-            if(list.Count >=4)
+            
+            if (list.Count >= 4)
             {
-                if(list[list.Count - 4] == 1 && list[list.Count - 3] == 2 && list[list.Count - 2] == 3 && list[list.Count - 1] == 1)
-                    {
+                int[] patternToMatch = { 1, 2, 3, 1 };
+        
+                if (list.Skip(list.Count - 4).SequenceEqual(patternToMatch))
+                {
                     answer++;
                     list.RemoveRange(list.Count - 4, 4);
                 }
-                    
             }
-            
         }
 
         return answer;
