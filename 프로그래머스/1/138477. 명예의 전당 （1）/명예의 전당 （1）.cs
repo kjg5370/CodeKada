22120d2b010eb2af120b299b23c5612420182740
kjg5370 <105926662+kjg5370@users.noreply.github.com>
2024-01-09 15:48:53 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.94 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.Length];
+        List<int> award = new List<int>();
+        int count = 0;
+        foreach(int s in score)
+        {
+            award.Add(s);
+            award.Sort();
+            if(award.Count>k)
+                award.RemoveAt(0);
+            answer[count++] = award[0];
+        }
+        return answer;
+    }
+}
